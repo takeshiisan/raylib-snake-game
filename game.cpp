55574ee63086d90b5efb7d1067261286a6e7bd64
takeshiisan <takeshisan29@gmail.com>
2025-06-02 15:34:28 +0800

    added retro background color and proper speed for snake

--- game.cpp
+++ game.cpp
@@ -3,12 +3,43 @@
 
 int main()
 {
-    InitWindow(800, 600, "Game");
+    const int screenWidth = 750;
+    const int screenHeight = 750;
+    
+    Color background = { 173, 204, 96, 255 };
+
+    int snakeX = screenWidth / 2;
+    int snakeY = screenHeight / 2;
+
+    int snakeSpeed = 10;
+
+    InitWindow(screenWidth, screenHeight, "Game");
     SetTargetFPS(60);
+
+
     while (WindowShouldClose() == false)
     {
+        if(IsKeyPressed(KEY_RIGHT))
+        {
+            snakeX += snakeSpeed;
+        }
+        if(IsKeyPressed(KEY_LEFT))
+        {
+            snakeX -= snakeSpeed;
+        }
+        if(IsKeyPressed(KEY_DOWN))
+        {
+            snakeY += snakeSpeed;
+        }
+        if(IsKeyPressed(KEY_UP))
+        {
+            snakeY -= snakeSpeed;
+        }
+
         BeginDrawing();
-        ClearBackground(RAYWHITE);
+        //Snake
+        DrawRectangle(snakeX, snakeY, 10, 10, RED);
+        ClearBackground(background);
         EndDrawing();
     }
     CloseWindow();
