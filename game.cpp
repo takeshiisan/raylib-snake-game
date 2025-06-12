bfef91f3e892a5cc7bd8bc5ccbe2cc6c1a88ec98
takeshiisan <takeshisan29@gmail.com>
2025-06-12 21:02:53 +0800

    randomly generated food

--- game.cpp
+++ game.cpp
@@ -19,24 +19,36 @@ int cellCount = 25;
 class Food{
     
     public: 
-        Vector2 position = {5, 6};
+        Vector2 position;
         Texture2D texture; 
 
+        //Constructor for food
         Food()
         {
             Image image = LoadImage("graphics/food.png");
             texture = LoadTextureFromImage(image);
             UnloadImage(image);
+            position = GenerateRandomPos();
         }
 
+        //Deconstructor
         ~Food()
         {
             UnloadTexture(texture);
         }
 
+        //Draws the food image
         void Draw() 
         {
             DrawTexture(texture, position.x * cellSize, position.y * cellSize, WHITE);
+        }
+
+        //Generate random position of the food that the snake will eat
+        Vector2 GenerateRandomPos()
+        {
+            float x = GetRandomValue(0, cellCount - 1);
+            float y = GetRandomValue(0, cellCount - 1);
+            return Vector2{x,y};
         }
 };
     int main()
